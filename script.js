6e7fd81273ff28ed50b142ffd08509a15a84914e
brunohssf <69315299+brunohssf@users.noreply.github.com>
2020-08-06 14:18:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-makers = [], lastTick = Date.now(), tickCount = 0;
+var makers = [], lastTick = Date.now(), tickCount = 0;
 var secondsPassed = 0, gameScore = 200, scoreSpeed = 0, oldScore = 0, body;
 let oldTimeStamp, timeStamp = new Date().getTime(), gameTime = 0;
 var timeFactor = 1/1000;
