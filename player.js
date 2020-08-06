ba04b2383266341c6d1dbd4bf4160be6a0e69757
brunohssf <69315299+brunohssf@users.noreply.github.com>
2020-08-06 14:49:19 -0300

    Changelog

--- player.js
+++ player.js
@@ -1,2 +1,9 @@
-var player = {};
 let w = window;
+var player = {}, makers = [], lastTick = Date.now(), tickCount = 0;
+var secondsPassed = 0, gameScore = 200, scoreSpeed = 0, oldScore = 0, body;
+let oldTimeStamp, timeStamp = new Date().getTime(), gameTime = 0;
+var timeFactor = 1/1000;
+var dimNum = 8;
+var logging = new Boolean(true);
+
+makers = player.makers;
--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-var makers = [], lastTick = Date.now(), tickCount = 0;
-var secondsPassed = 0, gameScore = 200, scoreSpeed = 0, oldScore = 0, body;
-let oldTimeStamp, timeStamp = new Date().getTime(), gameTime = 0;
-var timeFactor = 1/1000;
-var dimNum = 8;
-var logging = new Boolean(true);
-
 function makerBuilder(i) {
 	let maker = {
 	cost: 10 + 10000 * i ** (2 + 3 * i * i) - 9000 * i,
