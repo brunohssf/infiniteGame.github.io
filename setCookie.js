226772312fac28f3d24a49c389cdd61da830a230
brunohssf <69315299+brunohssf@users.noreply.github.com>
2020-08-07 10:27:38 -0300

    Update setCookie.js

--- setCookie.js
+++ setCookie.js
@@ -10,11 +10,8 @@ function getCookie(cname) {
 	var ca = document.cookie.split(';');
 	for(var i = 0; i < ca.length; i++) {
 		var c = ca[i];
-		while (c.charAt(0) == ' ') {
-			c = c.substring(1);
-		}
-		if (c.indexOf(name) == 0) {
-		return c.substring(name.length, c.length);
+		if (c.indexOf("infiniteGame") = 0) {
+		return c.substring(name.lenght+1);
 		}
 	}
 	return "";
